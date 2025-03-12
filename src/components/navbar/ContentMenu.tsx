cc8fc0ef5fb40085c7018ec6d5d265b5dad45e18
Glenn-Rhee <glennviktor5@gmail.com>
2025-03-13 01:58:09 +0700

    feat: added content menu navbar at mobilephone

--- src/components/navbar/ContentMenu.tsx
+++ src/components/navbar/ContentMenu.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { useNavbar } from "@/store/NavbarStore";
+import clsx from "clsx";
 import { motion } from "framer-motion";
 
 export default function ContentMenu() {
   const { isOpen } = useNavbar();
+  const menues = ["Home", "About", "Projects", "Contact"];
   return (
     <motion.div
       animate={isOpen ? "open" : "close"}
@@ -11,7 +13,7 @@ export default function ContentMenu() {
         open: {
           width: "100vw",
           height: "100vh",
-          opacity: 1,
+          opacity: 0.8,
           top: 0,
           left: 0,
           right: 0,
@@ -23,16 +25,47 @@ export default function ContentMenu() {
           left: "100vw",
           bottom: 0,
           right: 100,
+          opacity: 0.8,
           width: 0,
           height: 0,
-          opacity: 1,
           borderRadius: "50%",
         },
       }}
       transition={{
         duration: 0.5,
       }}
-      className="flex fixed bg-slate-600 lg:hidden"
-    ></motion.div>
+      className="flex flex-col gap-y-4 fixed justify-center items-center bg-slate-600 lg:hidden"
+    >
+      {isOpen ? (
+        <>
+          <motion.h3
+            animate={["open", "opened"]}
+            variants={{
+              open: {
+                x: -100,
+              },
+              opened: {
+                x: 0,
+              },
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 0.4,
+            }}
+            className={clsx("font-bold text-gray-100 text-5xl mb-4")}
+          >
+            Menu
+          </motion.h3>
+          {menues.map((v, i) => (
+            <span
+              key={i}
+              className="text-gray-100 cursor-pointer text-xl font-semibold"
+            >
+              {v}
+            </span>
+          ))}
+        </>
+      ) : null}
+    </motion.div>
   );
 }

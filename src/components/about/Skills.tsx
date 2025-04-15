88510846bb92e8e008488db52c0bbfda270bcd51
Glenn-Rhee <glennviktor5@gmail.com>
2025-04-15 23:35:41 +0700

    feat: added skills & expertise

--- src/components/about/Skills.tsx
+++ src/components/about/Skills.tsx
@@ -24,7 +24,7 @@ export default function Skills() {
     },
   ];
   return (
-    <div className="w-full flex mt-40 flex-col items-center justify-center lg:flex-row lg:items-center">
+    <div className="w-full flex mt-40 md:mt-16 flex-col items-center gap-y-28 justify-center">
       {/* Skills & Expertise */}
       <div className="flex mx-auto flex-col gap-y-3">
         <div className="space-y-3 text-center">
@@ -53,6 +53,40 @@ export default function Skills() {
               </span>
             </div>
           ))}
+        </div>
+      </div>
+
+      {/* Language & Frameworks */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+        <div className="space-y-4 flex flex-col items-center">
+          <h5 className="uppercase text-center text-lg md:text-xl lg:text-xl text-black-primary tracking-[0.4em]">
+            Language & Framework
+          </h5>
+          <span className="font-semibold text-slate-800 text-center">
+            <span className="font-bold text-black-primary">HTML</span> |
+            <span className="font-bold text-black-primary"> CSS</span> |
+            <span className="font-bold text-black-primary"> JavaScript</span> |
+            <span className="font-bold text-black-primary"> Typescript</span> |
+            <span className="font-bold text-black-primary"> ReactJs</span> |
+            <span className="font-bold text-black-primary"> NextJs</span> |
+            <span className="font-bold text-black-primary"> TailwindCSS</span> |
+            <span className="font-bold text-black-primary"> Bootstrap</span> |
+            <span className="font-bold text-black-primary"> NodeJs</span> |
+            <span className="font-bold text-black-primary"> ExpressJs</span> |
+            <span className="font-bold text-black-primary"> MySQL</span> |
+            <span className="font-bold text-black-primary"> MongoDB</span> |
+            firebase | Golang | PHP
+          </span>
+        </div>
+        <div className="space-y-4 flex flex-col items-center">
+          <h5 className="uppercase text-center text-lg md:text-xl lg:text-xl text-black-primary tracking-[0.4em]">
+            Tools
+          </h5>
+          <span className="font-bold text-black-primary text-center">
+            VsCode | Git | Github |{" "}
+            <span className="text-slate-800 font-semibold">Figma</span> |
+            Postman
+          </span>
         </div>
       </div>
     </div>

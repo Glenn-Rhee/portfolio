02b66a3be65780a4e178b95659d603b036920161
Glenn-Rhee <glennviktor5@gmail.com>
2025-05-02 16:17:44 +0700

    feat: added animation at otherproject

--- src/components/projects/OtherProject.tsx
+++ src/components/projects/OtherProject.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { variantsText } from "../MainPage";
+import { useEffect, useState } from "react";
+import { useNavbar } from "@/store/NavbarStore";
 
 interface Project {
   title: string;
@@ -8,62 +13,101 @@ interface Project {
   image: string;
 }
 
+const projects: Project[] = [
+  {
+    title: "Client Landing Page",
+    desc: "A landing page website that I created for my client. This website is built using three tech, native (HTML, CSS, JS)",
+    techs: ["HTML", "CSS", "JS"],
+    year: 2025,
+    image: "/projects/p-3.jpg",
+  },
+  {
+    title: "Coffe Shop",
+    desc: "A simple coffee ordering app built with NextJs, Typescript, Prisma, shadcnUI, and TailwindCss.",
+    techs: ["NextJs", "Typescript", "Prisma", "shadcnUI", "TailwindCss"],
+    year: 2024,
+    image: "/projects/p-5.png",
+  },
+  {
+    title: "Netflix Landing Page",
+    desc: "A landing page website that I created for my study. This website is built using HTML, tailwindCss, and JS",
+    techs: ["HTML", "TailwindCss", "JS"],
+    year: 2023,
+    image: "/projects/p-4.jpg",
+  },
+  {
+    title: "Casecobra",
+    desc: "A simple application for adding and managing phone cases. This website uses NextJs, Typescript, Prisma, TailwindCss",
+    techs: ["NextJs", "Typescript", "Prisma", "TailwindCss"],
+    year: 2024,
+    image: "/projects/p-6.jpg",
+  },
+  {
+    title: "Tic Tac Toe Game",
+    desc: "A simple tic tac toe game that I created for my study. This website is built using ReactJs, tailwindCss, and JS",
+    techs: ["ReactJs", "TailwindCss", "JS"],
+    year: 2023,
+    image: "/projects/p-7.jpg",
+  },
+  {
+    title: "Landing page",
+    desc: "A simple landing page that I created for my study. This website is built using HTML, CSS, and JS",
+    techs: ["HTML", "CSS", "JS"],
+    year: 2023,
+    image: "/projects/p-8.jpg",
+  },
+];
 export default function OtherProject() {
-  const projects: Project[] = [
-    {
-      title: "Client Landing Page",
-      desc: "A landing page website that I created for my client. This website is built using three tech, native (HTML, CSS, JS)",
-      techs: ["HTML", "CSS", "JS"],
-      year: 2025,
-      image: "/projects/p-3.jpg",
-    },
-    {
-      title: "Coffe Shop",
-      desc: "A simple application for ordering coffee. This website uses NextJs, Typescript, Prisma, shadcnUI, and TailwindCss.",
-      techs: ["NextJs", "Typescript", "Prisma", "shadcnUI", "TailwindCss"],
-      year: 2024,
-      image: "/projects/p-5.png",
-    },
-    {
-      title: "Netflix Landing Page",
-      desc: "A landing page website that I created for my study. This website is built using HTML, tailwindCss, and JS",
-      techs: ["HTML", "TailwindCss", "JS"],
-      year: 2023,
-      image: "/projects/p-4.jpg",
-    },
-    {
-      title: "Casecobra",
-      desc: "A simple application for adding and managing phone cases. This website uses NextJs, Typescript, Prisma, TailwindCss",
-      techs: ["NextJs", "Typescript", "Prisma", "TailwindCss"],
-      year: 2024,
-      image: "/projects/p-6.jpg",
-    },
-    {
-      title: "Tic Tac Toe Game",
-      desc: "A simple tic tac toe game that I created for my study. This website is built using ReactJs, tailwindCss, and JS",
-      techs: ["ReactJs", "TailwindCss", "JS"],
-      year: 2023,
-      image: "/projects/p-7.jpg",
-    },
-    {
-      title: "Landing page",
-      desc: "A simple landing page that I created for my study. This website is built using HTML, CSS, and JS",
-      techs: ["HTML", "CSS", "JS"],
-      year: 2023,
-      image: "/projects/p-8.jpg",
-    },
-  ];
+  const { gapFromTop } = useNavbar();
+  const [isShow, setIsShow] = useState(gapFromTop > 570);
 
+  useEffect(() => {
+    if (gapFromTop > 570) {
+      setIsShow(true);
+    } else {
+      setIsShow(false);
+    }
+  }, [gapFromTop]);
   return (
     <div className="ms-2 md:ms-0 pb-10 w-full md:mt-0">
-      <h3 className="font-bold md:text-start text-center text-2xl lg:text-3xl">
+      <motion.h3
+        initial={{
+          x: -40,
+          opacity: 0,
+        }}
+        variants={variantsText}
+        animate={isShow ? "show" : "hide"}
+        transition={{
+          duration: 0.5,
+          ease: "easeOut",
+          delay: 0,
+        }}
+        className="font-bold md:text-start text-center text-2xl lg:text-3xl"
+      >
         Other Projects
-      </h3>
+      </motion.h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-4">
         {projects
           .sort((a, b) => b.year - a.year)
           .map((project, i) => (
-            <div key={i} className="relative overflow-hidden cursor-pointer">
+            <motion.div
+              key={i}
+              initial={{
+                y: 40,
+                opacity: 0,
+              }}
+              variants={{
+                show: { y: 0, opacity: 1 },
+                hide: { y: 40, opacity: 0 },
+              }}
+              animate={isShow ? "show" : "hide"}
+              transition={{
+                duration: 0.5,
+                ease: "easeOut",
+                delay: 0.4 + i * 0.2,
+              }}
+              className="relative overflow-hidden cursor-pointer"
+            >
               <Image
                 src={project.image}
                 alt={`Project ${i + 1} Ariel`}
@@ -74,7 +118,7 @@ export default function OtherProject() {
               <div className="py-2 z-20 px-2 font-semibold text-lg bg-slate-600 text-white absolute top-0 left-0">
                 {project.year}
               </div>
-              <div className="absolute px-10 hover:opacity-0 transition-all duration-300 top-0 right-0 left-0 bottom-0 bg-gray-300/85 flex flex-col gap-y-1 md:gap-y-3 items-center justify-center">
+              <div className="absolute px-10 hover:opacity-0 transition-all duration-300 top-0 right-0 left-0 bottom-0 bg-gray-400/85 flex flex-col gap-y-1 md:gap-y-3 items-center justify-center">
                 <h1 className="text-black-primary text-center text-xl lg:text-3xl font-semibold">
                   {project.title}
                 </h1>
@@ -92,7 +136,7 @@ export default function OtherProject() {
                   ))}
                 </div>
               </div>
-            </div>
+            </motion.div>
           ))}
       </div>
     </div>

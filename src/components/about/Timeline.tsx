9ec6ca07173c9a3549e996b2b6be6309830a5f1a
Glenn-Rhee <glennviktor5@gmail.com>
2025-09-28 18:09:47 +0700

    feat: remove Timeline on mobile

--- src/components/about/Timeline.tsx
+++ src/components/about/Timeline.tsx
@@ -1,5 +1,5 @@
 export default function Timeline() {
   return (
-    <div className="w-2 rounded-xs bg-gradient-to-b from-slate-900 via-slate-800 to-transparent h-full absolute left-1/2 -top-1/2 translate-1/2"></div>
+    <div className="w-2 hidden lg:block rounded-xs bg-gradient-to-b from-slate-900 via-slate-800 to-transparent h-full absolute left-1/2 -top-1/2 translate-1/2"></div>
   );
 }

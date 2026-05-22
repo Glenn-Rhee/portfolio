215d6d35f1964734aacac38e182ef264b0b773bf
Ariel Rizki <135257091+Glenn-Rhee@users.noreply.github.com>
2026-05-22 19:41:55 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import Projects from "@/components/Projects";
 
 type SearchParams = Promise<{ [key: string]: string | undefined }>;
 
+export 
+
 export default async function HomePage(props: { searchParams: SearchParams }) {
   const params = await props.searchParams;
 

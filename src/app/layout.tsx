c591e13e9bfc908573a4cfba81c369a7247ce081
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-18 20:21:57 +0700

    feat: added url at opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Ariel | Fullstack Web Developer Portfolio",
     description: "Portfolio Ariel Rizki Muhtamad Bakri...",
+    url: "https://aaarrl.my.id/",
   },
   description:
     "My name is Ariel, I'm passionate about fullstack web developer. I'm currently studying at Gunadarma University.",

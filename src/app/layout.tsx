a96a1eb3b50a561736e2e90ed918d7939805cf36
Glenn-Rhee <glennviktor5@gmail.com>
2026-08-18 20:21:17 +0700

    feat: added title at opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,10 @@ export const metadata: Metadata = {
     },
   },
   alternates: {
-    canonical: "/", 
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Ariel | Fullstack Web Developer Portfolio",
   },
   description:
     "My name is Ariel, I'm passionate about fullstack web developer. I'm currently studying at Gunadarma University.",
